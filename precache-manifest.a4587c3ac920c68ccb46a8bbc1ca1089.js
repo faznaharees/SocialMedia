7aface3244b93a144cd67281efd191d7b90b604f
Fazna <faznaharees@cet.ac.in>
2020-06-27 23:35:47 +0530

    Updates

---
+++ precache-manifest.a4587c3ac920c68ccb46a8bbc1ca1089.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce69a9b00760bfb8db47574f767bed24",
+    "url": "/SocialMedia/index.html"
+  },
+  {
+    "revision": "af962895e45e9c57eee7",
+    "url": "/SocialMedia/static/css/main.4f01f080.chunk.css"
+  },
+  {
+    "revision": "ce93ccdc2b88d2b7341e",
+    "url": "/SocialMedia/static/js/2.4c822231.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/SocialMedia/static/js/2.4c822231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af962895e45e9c57eee7",
+    "url": "/SocialMedia/static/js/main.c9a2b883.chunk.js"
+  },
+  {
+    "revision": "69d93e92030c32c150e8",
+    "url": "/SocialMedia/static/js/runtime-main.6931e6ba.js"
+  }
+]);
